e41c05c0ae5361bd47455c5479b809abe54908b5
Vadim Ivanov <sferamail33@yandex.ru>
2016-12-10 23:03:23 +0300

    Добавлено воспроизведение по нажатию на кнопки

--- 01 - Drum kit/index.js
+++ 01 - Drum kit/index.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
-  
+  window.addEventListener('keydown', (evt) => {
+    const code = evt.keyCode;
+    const audio = document.querySelector(`audio[data-key="${code}"]`);
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+  });
 });
