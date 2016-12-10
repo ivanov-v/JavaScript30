fd38ed7d8d7aba608b703e0e9115cb421fe471fd
Vadim Ivanov <sferamail33@yandex.ru>
2016-12-10 23:23:15 +0300

    Обработчики вынесены в отдельные функции

--- 01 - Drum kit/index.js
+++ 01 - Drum kit/index.js
@@ -1,11 +1,28 @@
 window.addEventListener('load', () => {
-  window.addEventListener('keydown', (evt) => {
+  const keydownHandler = function(evt) {
     const code = evt.keyCode;
     const audio = document.querySelector(`audio[data-key="${code}"]`);
+    const key = document.querySelector(`.key[data-key="${code}"]`);
 
     if (!audio) return;
 
     audio.currentTime = 0;
     audio.play();
+    
+    key.classList.add('play');
+  };
+  
+  const transitionendHandler = function(evt) {
+    if (evt.propertyName !== 'transform') return;
+    this.classList.remove('play');
+  };
+
+  const keys = document.querySelectorAll('.key');
+  
+  [].forEach.call(keys, (key) => {
+    key.addEventListener('transitionend', transitionendHandler);
+    key.addEventListener('click', transitionendHandler);
   });
+  
+  window.addEventListener('keydown', keydownHandler);
 });
