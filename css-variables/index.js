9340b0e53a4215b7f3c99dff565bc13f30850fb9
Vadim Ivanov <sferamail33@yandex.ru>
2017-01-28 12:23:45 +0300

    Добавлен обработчик изменения значений

--- css-variables/index.js
+++ css-variables/index.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
+  const inputs = document.querySelectorAll('input');
   
+  inputs.forEach(input => input.addEventListener('mousemove', handler));
+  inputs.forEach(input => input.addEventListener('change', handler));
+  
+  function handler() {
+    const name = this.name;
+    const value = this.value;
+    const sizing = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${name}`, `${value}${sizing}`);
+  }
 });
